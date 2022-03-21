c6ac7d16861bd6d842f53c2837c70348273cc165
zsofia_mandi <mandi.zsofia@gmail.com>
2022-03-21 17:54:40 +0100

    Checking if Location Provider is turned on

--- app/src/main/java/zsofi/applications/weatherapp/MainActivity.kt
+++ app/src/main/java/zsofi/applications/weatherapp/MainActivity.kt
@@ -1,11 +1,65 @@
 package zsofi.applications.weatherapp
 
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import zsofi.applications.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        if (isLocationEnabled()){
+            Toast.makeText(this,
+                "Your location is already turned on.",
+                Toast.LENGTH_SHORT).show()
+        }else{
+            showRationalDialogForLocation()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean{
+        // This provides access to the system location services
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showRationalDialogForLocation() {
+        AlertDialog.Builder(this, R.style.myDialogTheme).setMessage(
+            "It looks like you have turned off your location provider. Please turn it on."
+        )
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+
+            }.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    override fun onDestroy() {
+        if (binding != null){
+            binding = null
+        }
+        super.onDestroy()
     }
 }
